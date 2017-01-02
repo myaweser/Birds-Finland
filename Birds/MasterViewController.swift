0703fd9ae719e9505099bd5a275fbcf0546b9ecb
Oliver Kulpakko <oliver@kulpakko.com>
2017-01-02 17:17:24 +0200

    Add images to table view.

--- Birds/MasterViewController.swift
+++ Birds/MasterViewController.swift
@@ -101,9 +101,35 @@ class MasterViewController: UITableViewController {
 
         let bird = birds[indexPath.row]
         cell.textLabel!.text = bird.finnishName
+        
+        let image = UIImage(named: "\(bird.latinName).jpg")
+        cell.imageView?.image = self.cropImageToSquare(image: image!)
         return cell
     }
-
-
+    
+    func cropImageToSquare(image: UIImage) -> UIImage? {
+        var imageHeight = image.size.height
+        var imageWidth = image.size.width
+        
+        if imageHeight > imageWidth {
+            imageHeight = imageWidth
+        } else {
+            imageWidth = imageHeight
+        }
+        
+        let size = CGSize(width: imageWidth, height: imageHeight)
+        
+        let refWidth : CGFloat = CGFloat(image.cgImage!.width)
+        let refHeight : CGFloat = CGFloat(image.cgImage!.height)
+        
+        let x = (refWidth - size.width) / 2
+        let y = (refHeight - size.height) / 2
+        
+        let cropRect = CGRect(x: x, y: y, width: size.height, height: size.width)
+        if let imageRef = image.cgImage!.cropping(to: cropRect) {
+            return UIImage(cgImage: imageRef, scale: 0, orientation: image.imageOrientation)
+        }
+        
+        return nil
+    }
 }
-
