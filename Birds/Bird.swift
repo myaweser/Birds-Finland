2b75639eff249a7b3273b2bd0764927bf4f431dc
Oliver Kulpakko <oliver@kulpakko.com>
2017-01-02 17:06:58 +0200

    Download and add birds to table view.

---
+++ Birds/Bird.swift
@@ -0,0 +1,19 @@
+//
+//  Bird.swift
+//  Birds
+//
+//  Created by Oliver Kulpakko on 2017-01-02.
+//  Copyright © 2017 East Studios. All rights reserved.
+//
+
+import Foundation
+
+class Bird {
+    var internalName = ""
+    var latinName = ""
+    var englishName = ""
+    var finnishName = ""
+    var swedishName = ""
+    var description = ""
+    var author = ""
+}
--- Birds/MasterViewController.swift
+++ Birds/MasterViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
+    var birds = [Bird]()
 
 
     override func viewDidLoad() {
@@ -22,6 +22,8 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
+        getBirds()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -34,14 +36,50 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getBirds() {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        if let url = NSURL(string: "https://eaststudios.fi/api/BirdsFI/v1/getBirds") {
+            session.dataTask(with: url as URL, completionHandler: { (data, response, error) -> Void in
+                if error != nil {
+                    print("Can't get Birds. Error: \(error!)")
+                    return
+                } else if let jsonData = data {
+                    do {
+                        let parsedJSON = try JSONSerialization.jsonObject(with: jsonData, options: []) as! [String:AnyObject]
+                        guard let results = parsedJSON["birds"] as? [[String:AnyObject]] else { return }
+                        for result in results {
+                            let newBird = Bird()
+                            newBird.internalName = result["internalName"] as! String
+                            newBird.latinName = result["latinName"] as! String
+                            newBird.englishName = result["englishName"] as! String
+                            newBird.finnishName = result["finnishName"] as! String
+                            newBird.swedishName = result["swedishName"] as! String
+                            newBird.description = result["description"] as! String
+                            newBird.author = result["author"] as! String
+                            
+                            self.birds.append(newBird)
+                        }
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                        
+                    } catch let error as NSError {
+                        print(error)
+                    }
+                }
+            }).resume()
+        }
+    }
+    
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let selectedBird = birds[indexPath.row] 
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = selectedBird
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -55,14 +93,14 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return birds.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let bird = birds[indexPath.row]
+        cell.textLabel!.text = bird.finnishName
         return cell
     }
 
