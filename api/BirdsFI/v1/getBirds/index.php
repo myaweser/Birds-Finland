15df562096a7adc959b55e2ed9ba106d94e70eb2
Oliver Kulpakko <oliver@kulpakko.com>
2017-01-05 11:19:00 +0200

    Add api to source control. Add placeholder image.

---
+++ api/BirdsFI/v1/getBirds/index.php
@@ -0,0 +1,29 @@
+<?php
+$appVersion =  $_GET['appVersion'];
+
+if($appVersion == "1.0") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else if($appVersion == "1.0.1") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else if($appVersion == "1.0.2") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else if($appVersion == "1.0.3") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else if($appVersion == "1.0.4") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else if($appVersion == "1.0.5") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else if($appVersion == "1.1") {
+    header("location: json/05-01-2017.json");
+    exit();
+} else {
+    header("location: json/unknownVersion.json");
+    exit();
+}
+?>
